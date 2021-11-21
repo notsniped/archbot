8b0060c03090ed5a6288ac71a5391ec7ab31b2ae
thatOneArchUser <69575053+thatOneArchUser@users.noreply.github.com>
2021-11-21 23:33:46 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -91,36 +91,6 @@ async def on_ready():
     print('------------------')
 
 @client.event
-async def on_message_delete(message):
-        if message.content.startswith(".say"):
-            return
-        else:
-            pass
-        if not message.author.bot:
-            pass
-        else:
-            return
-        now = datetime.datetime.now()
-        current_time = now.strftime("%H:%M:%S")
-        guild = client.guilds[0]
-        channel = message.channel
-        snipe_message_author[message.channel.id] = message.author
-        snipe_message_content[message.channel.id] = message.content
-        if bool(log) == True:
-        # with open('F:\\bot\\logs\\log.txt', 'a') as f:
-        #     f.write(f'[{current_time}]Message deleted by {snipe_message_author[channel.id]}.\n   Content:{snipe_message_content[channel.id]}\n')
-        #     f.close()
-            if message.guild.id == 876826249207640096:
-                c = client.get_channel(881181406582165525)
-                em = discord.Embed(name = f"Last deleted message in #{channel.name}", description = snipe_message_content[channel.id])
-                em.set_footer(text = f"This message was sent by {snipe_message_author[channel.id]}")
-                await c.send(embed = em)
-            else:
-                pass
-        else:
-            pass
-
-@client.event
 async def on_message_edit(message_before, message_after):
         global author
         author = message_before.author

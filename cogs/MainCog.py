20a7c6c0bd90b913bb8b4e6b7c99bc0e006aafbe
thatOneArchUser <69575053+thatOneArchUser@users.noreply.github.com>
2021-11-21 23:11:55 +0200

    Update MainCog.py

--- cogs/MainCog.py
+++ cogs/MainCog.py
@@ -513,7 +513,7 @@ class MainCog(commands.Cog):
     ]
         rndcol = 0xff0000
         if arg1 == None:
-            helpMain = discord.Embed(title='**COMMAND LIST**', description='Economy\nbeg, balance, daily, weekly, monthly, postmeme, work, guess, give, deposit, withdraw, shop, buy, inventory\n\nModeration\nban, kick, purge, nuke, snipe, warns\n\nMisc\nmeme, linuxmeme, softwaregore, ihadastroke, stroke, say, edit_snipe, 8ball, lbin, auctionhouse')
+            helpMain = discord.Embed(title='**COMMAND LIST**', description='Economy\nbeg, balance, daily, weekly, monthly, postmeme, work, guess, give, deposit, withdraw, shop, buy, inventory, passive\n\nModeration\nban, kick, purge, nuke, snipe, warns\n\nMisc\nmeme, linuxmeme, softwaregore, ihadastroke, stroke, say, edit_snipe, 8ball, lbin, auctionhouse')
             helpMain.set_footer(text='*type .help [command] to get more info about a command*')
             await ctx.reply(embed = helpMain)
         elif arg1 == 'beg':
@@ -1149,7 +1149,7 @@ class MainCog(commands.Cog):
         now = datetime.datetime.now()
         current_time = now.strftime("%H:%M:%S")
         coins = randint(1000, 50000)
-        wallet[user.id] += coins
+        wallet[ctx.message.author.id] += coins
         await ctx.send(f"You earned {coins} coins.")
         self.save()
         if bool(log) == True:

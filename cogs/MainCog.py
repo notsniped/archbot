2e5e3779a9d22bbf3cfcfb7e0225b9454b8af557
thatOneArchUser <69575053+thatOneArchUser@users.noreply.github.com>
2021-11-28 11:24:49 +0200

    Update MainCog.py

--- cogs/MainCog.py
+++ cogs/MainCog.py
@@ -208,8 +208,8 @@ class MainCog(commands.Cog):
     
     @commands.command()
     async def credits(self, ctx):
-        em = discord.Embed(title="Arch bot developers team", description="thatOneArchUser#5794, Main developer\nnotsniped#0069, made \"purge\" command\nMarios1Gr#3949, made deposit/withdraw\nαrchιshα#5518, tester\nnexus#1047, tester")
-        await ctx.reply(embed=em, mention_author=False, hidden=True)
+        em = discord.Embed(title="Arch bot developers team", description="thatOneArchUser#5794, Main developer\nnotsniped#0002, made \"purge\" command\nMarios1Gr#3949, made deposit/withdraw\nαrchιshα#5518, tester\nnexus#1047, tester")
+        await ctx.reply(embed=em, mention_author=False)
     
     @commands.command(aliases=["vs"])
     async def viewsettings(self, ctx):

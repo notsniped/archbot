7ede07630f9f5a9440667b0ba5dc0233b770c9c8
thatOneArchUser <69575053+thatOneArchUser@users.noreply.github.com>
2021-11-29 22:22:23 +0200

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -8,7 +8,7 @@ def main():
     return "Your bot is alive!"
 
 def run():
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="94.69.117.121", port=8080)
 
 def keep_alive():
     server = Thread(target=run)
